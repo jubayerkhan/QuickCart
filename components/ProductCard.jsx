89fb582bf8c3633865a357a7bcd8e591cb41cc3f
@jubayer.khan <jubayer.khan@annanovas.com>
2026-03-11 11:43:31 +0600

    mongoose

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -10,13 +10,13 @@ const ProductCard = ({ product }) => {
     return (
         <div
             onClick={() => { router.push('/product/' + product._id); scrollTo(0, 0) }}
-            className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer"
+            className="flex flex-col items-start gap-0.5 w-full cursor-pointer"
         >
             <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
                 <Image
                     src={product.image[0]}
                     alt={product.name}
-                    className="group-hover:scale-105 transition object-cover w-4/5 h-4/5 md:w-full md:h-full"
+                    className="group-hover:scale-105 transition object-cover w-4/5 h-4/5 md:w-full md:h-full 2xl:px-10"
                     width={800}
                     height={800}
                 />
