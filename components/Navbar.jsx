251d920195d0570a732478147711aa31b40aad68
@jubayer.khan <jubayer.khan@annanovas.com>
2026-03-11 09:50:46 +0600

    clerk

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { assets, CartIcon } from "@/assets/assets";
+import { assets, BoxIcon, CartIcon, HomeIcon } from "@/assets/assets";
 import Link from "next/link";
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
@@ -48,7 +48,18 @@ const Navbar = () => {
           <>
             <UserButton>
               <UserButton.MenuItems>
-                <UserButton.Action label="Cart" labelIcon={ <CartIcon />} onClick={() => router.push('/cart')}/>
+                <UserButton.Action
+                  label="Cart"
+                  labelIcon={<CartIcon />}
+                  onClick={() => router.push("/cart")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="My Orders"
+                  labelIcon={<CartIcon />}
+                  onClick={() => router.push("/my-orders")}
+                />
               </UserButton.MenuItems>
             </UserButton>
           </>
@@ -72,13 +83,48 @@ const Navbar = () => {
             Seller Dashboard
           </button>
         )}
-        <button
-          onClick={openSignIn}
-          className="flex items-center gap-2 hover:text-gray-900 transition"
-        >
-          <Image src={assets.user_icon} alt="user icon" />
-          Account
-        </button>
+        {user ? (
+          <>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Home"
+                  labelIcon={<HomeIcon />}
+                  onClick={() => router.push("/")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Products"
+                  labelIcon={<BoxIcon />}
+                  onClick={() => router.push("/all-products")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Cart"
+                  labelIcon={<CartIcon />}
+                  onClick={() => router.push("/cart")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="My Orders"
+                  labelIcon={<CartIcon />}
+                  onClick={() => router.push("/my-orders")}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
       </div>
     </nav>
   );
