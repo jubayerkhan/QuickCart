61877d7a0617bb1e06609e5975bced20f86318b3
@jubayer.khan <jubayer.khan@annanovas.com>
2026-03-31 16:44:48 +0600

    env

--- config/inngest.js
+++ config/inngest.js
@@ -2,3 +2,44 @@ import { Inngest } from "inngest";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
+
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: "sync-user-from-clerk",
+    },
+    {
+        event: "clerk.user.created",
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_address, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + " " + last_name,
+            imageUrl: image_url,
+        };
+        await connectDB();
+        await User.create(userData);
+    }
+)
+
+// ingest function to update user data when user is updated in clerk
+export const syncUserUpdate = inngest.createFunction(
+    {
+        id: "update-user-from-clerk",
+    },
+    {
+        event: "clerk/user.updated",
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_address, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + " " + last_name,
+            imageUrl: image_url,
+        };
+        await connectDB();
+        await User.findByIdAndUpdate(id, userData);
+    }
+)
